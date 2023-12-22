6a791ae88be66221520e7dd754bfeb0a2061d92a
chrezalvin <67034165+chrezalvin@users.noreply.github.com>
2023-12-22 09:47:07 +0700

    update UI

--- SMS Interactive Dating Story/Assets/Scripts/PlayerStuff.cs
+++ SMS Interactive Dating Story/Assets/Scripts/PlayerStuff.cs
@@ -8,6 +8,26 @@ public class PlayerStuff : MonoBehaviour
     [SerializeField] private int money = 100000;
     [SerializeField] private UIController uiController;
 
+    [SerializeField] private float maxHeart = 100f;
+    [SerializeField] private float currentHeart = 20f;
+
+    [SerializeField] private int startHour = 8;
+    [SerializeField] private int endHour = 22;
+
+    private int currentHour = 8;
+
+    public void UpdateHeart(float amount)
+    {
+        currentHeart += amount;
+        currentHeart = Mathf.Clamp(currentHeart, 0f, maxHeart);
+        uiController.UpdateHeartDisplay(currentHeart, maxHeart);
+    }
+
+    public float GetHeart()
+    {
+        return currentHeart;
+    }
+
     public void AddItem(Item item)
     {
         stuff.Add(item);
@@ -21,6 +41,7 @@ public class PlayerStuff : MonoBehaviour
     public void AddMoney(int amount)
     {
         money += amount;
+        uiController.UpdateMoneyDisplay(money);
     }
 
     public void RemoveMoney(int amount)
@@ -29,6 +50,7 @@ public class PlayerStuff : MonoBehaviour
             return;
 
         money -= amount;
+        uiController.UpdateMoneyDisplay(money);
     }
 
     public int GetMoney()
@@ -36,10 +58,22 @@ public class PlayerStuff : MonoBehaviour
         return money;
     }
 
+    public void AddHour(int hour)
+    {
+        currentHour = Mathf.Clamp(currentHour + hour, startHour, endHour);
+    }
+
+    public int GetHour()
+    {
+        return currentHour;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         uiController.UpdateMoneyDisplay(money);
+        uiController.UpdateHeartDisplay(currentHeart, maxHeart);
+        uiController.UpdateTimeDisplay(currentHour);
     }
 
     // Update is called once per frame
--- SMS Interactive Dating Story/Assets/Scripts/UI/UIController.cs
+++ SMS Interactive Dating Story/Assets/Scripts/UI/UIController.cs
@@ -2,16 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     public TextMeshProUGUI moneyDisplay;
+    public Slider heartSlider;
+    public TextMeshProUGUI timeDisplay;
+
+    public void UpdateHeartDisplay(float currentHeart, float maxHeart)
+    {
+        heartSlider.value = currentHeart / maxHeart;
+    }
 
     public void UpdateMoneyDisplay(int money)
     {
         // format to like Rp. 100,000,000
         string format = string.Format("Rp. {0:N0}", money);
         moneyDisplay.text = format;
+    }
+
+    public void UpdateTimeDisplay(int hour)
+    {
+        // format to like 0[hour]:[real world minute] AM/PM
+        // get minute from real world
+        int minute = System.DateTime.Now.Minute;
+        string format = string.Format("{0:00}:{1:00} {2}", hour, minute, hour < 12 ? "AM" : "PM");
+        timeDisplay.text = format;
     }
 
     // Start is called before the first frame update
